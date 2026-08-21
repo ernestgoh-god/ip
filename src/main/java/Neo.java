5f23f8e8f02d96e667ae6e7610b6423fc52fa2fe
Ernest Goh <ernestgoh01@gmail.com>
2026-08-21 15:36:53 +0800

    Rename chatbot to Neo, update documentation and add Neo greeting and farewell

--- src/main/java/Neo.java
+++ src/main/java/Neo.java
@@ -8,6 +8,14 @@ public class Neo {
                 + "|  \\| |/ _ \\/ _ \\\n"
                 + "| |\\  |  __/ (_) |\n"
                 + "|_| \\_|\\___|\\___/ \n";
-        System.out.println(banner);
+        String separator = "_".repeat(60);
+
+        System.out.println(separator);
+        System.out.print(banner);
+        System.out.println("Hello! I'm Neo.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
